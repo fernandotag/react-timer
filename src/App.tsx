d2466bac1a26fc993fea3eb1ea0a42f1699a0ee3
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2022-12-20 22:58:52 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components'
 import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
-import defaultTheme from './styles/themes/default'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
-import { History } from './components/History'
-import { Home } from './components/Home'
+import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { Home } from './pages/Home'
 
 export function Router() {
   return (
