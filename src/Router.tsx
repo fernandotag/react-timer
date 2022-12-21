e30ecc85e06f77e51ef648ec427bd1946660c2bc
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2022-12-20 23:23:32 -0300

    feat: create home component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { Home } from './pages/Home'
+import { Home } from './components/Home'
 
 export function Router() {
   return (
